81a3fa485e3d9ae31622230e82a629e796b4e64f
sunset12321215 <sunsetno08@gmail.com>
2020-09-16 11:07:33 +0700

    Initial Commit

---
+++ MultiLanguage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultiLanguage
+//
+//  Created by Vu Xuan Cuong on 9/16/20.
+//  Copyright © 2020 Vu Xuan Cuong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
